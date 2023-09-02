8ffd57012996d62e8a7adf7828daa161d42d8843
shifna <shifna@luminartechnolab.com>
2023-09-02 19:03:59 +0530

    first

---
+++ src/app/bankService/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  data="data inside service file"
+
+  serviceMethod(){
+    alert("service method")
+  }
+
+
+}
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  data="happy banking with us"
+  data2="enter account number"
+
+  acno:any
+  psw:any
+
+  constructor(private rout:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+
+    // console.log(this.acno);
+    // console.log(this.psw);    
+    // // alert(this.acno)
+
+    // redierection
+    this.rout.navigateByUrl("home")
+
+
+    // api call
+
+  }
+
+
+
+
+
+}
